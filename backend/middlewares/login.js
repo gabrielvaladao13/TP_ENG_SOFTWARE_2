d124db62c213eea4908f9adf80497fccb252233e
Gabriel Valadão Meira <gabriel.valadao@sedecam>
2023-11-02 19:31:45 -0300

    Refatoração função isAdmin: Simplificação condicional e na função notLoggedIn: Melhoria da estrutura do código para tratamento de erro

--- backend/middlewares/login.js
+++ backend/middlewares/login.js
@@ -1,96 +1,92 @@
 const jwt = require('jsonwebtoken');
 const passport = require('passport');
-const {verify} = require('jsonwebtoken');
+const { verify } = require('jsonwebtoken');
 
+// Middleware para autenticar o usuário
 function loginMiddleware(req, res, next) {
-  passport.authenticate(
-    'login',
-    (err, user, info) => {
-      try {
-        if (err) {
-          return next(err);
-        }
-
-        req.login(
-          user,
-          {session: false},
-          (error) => {
-            if (error) next(error);
-
-            const body = {
-              id: user.id,
-              role: user.role,
-            };
-
-            const token = jwt.sign({user: body}, 'mysecretkey',
-              {expiresIn: '15d'});
-
-            res.cookie('jwt', token, {
-              httpOnly: true
-              //secure: 'development' = 'production',
-            });
-
-            res.status(204).end();
-          },
-
-        );
-      } catch (error) {
-        next(error);
-      }
-    },
-  )(req, res, next);
-};
-function cookieExtractor(req) {
-    let token = null;
-  
-    if (req && req.cookies) {
-      token = req.cookies['jwt'];
+  passport.authenticate('login', (err, user, info) => {
+    if (err) {
+      return next(err);
     }
-  
-    return token;
-  }
-function jwtMiddleware(req, res, next) {
-    try {
-      const token = cookieExtractor(req);
-      if (token) {
-        const decoded = verify(token,'mysecretkey');
-        req.user = decoded.user;
+
+    req.login(user, { session: false }, (error) => {
+      if (error) {
+        return next(error);
       }
-  
-      if (!req.user) {
-        throw new Error(
-          'Você precisa estar logado para realizar essa ação!');
-      }
-      next();
-    } catch (error) {
-      next(error);
-    }
-  }
-function notLoggedIn(req, res, next) {
-    const token = req.cookies['jwt'];
-    if (token) {
-      jwt.verify(token, 'mysecretkey', (err, decoded) => {
-        if (!(err instanceof jwt.TokenExpiredError)) {
-          res.status(400).send('Voce ja esta logado no sistema');
-        }
+
+      const body = {
+        id: user.id,
+        role: user.role,
+      };
+
+      const token = jwt.sign({ user: body }, 'mysecretkey', { expiresIn: '15d' });
+
+      res.cookie('jwt', token, {
+        httpOnly: true,
+        // secure: process.env.NODE_ENV === 'production',
       });
-    }
-  
-  next();
-  };
 
-  // Função para verificar se o usuário é admin
-function isAdmin(req, res, next) {
-    if (req.user.role !== 'admin') {
-      res.status(401).send('Você não tem permissão para realizar essa ação!');
+      res.status(204).end();
+    });
+  })(req, res, next);
+}
+
+function cookieExtractor(req) { 
+  let token = null; 
+ 
+  if (req && req.cookies) { 
+    token = req.cookies['jwt']; 
+  } 
+ 
+  return token; 
+} 
+
+// Middleware para verificar o token JWT
+function jwtMiddleware(req, res, next) {
+  try {
+    const token = cookieExtractor(req);
+
+    if (!token) {
+      throw new Error('Você precisa estar logado para realizar essa ação!');
     }
-  
+
+    const decoded = verify(token, 'mysecretkey');
+    req.user = decoded.user;
+
     next();
+  } catch (error) {
+    next(error);
   }
+}
+
+// Middleware para verificar se o usuário não está logado
+function notLoggedIn(req, res, next) {
+  const token = req.cookies['jwt'];
+
+  if (token) {
+    jwt.verify(token, 'mysecretkey', (err, decoded) => {
+      if (!(err instanceof jwt.TokenExpiredError)) {
+        return res.status(400).send('Você já está logado no sistema');
+      }
+    });
+    
+  }
+
+  next();
+}
+
+// Middleware para verificar se o usuário é admin
+function isAdmin(req, res, next) {
+  if (req.user && req.user.role === 'admin') {
+    return next();
+  }
+
+  res.status(401).send('Você não tem permissão para realizar essa ação!');
+}
 
 module.exports = {
-    loginMiddleware,
-    jwtMiddleware,
-    notLoggedIn,
-    isAdmin
-  };
+  loginMiddleware,
+  jwtMiddleware,
+  notLoggedIn,
+  isAdmin,
+};
