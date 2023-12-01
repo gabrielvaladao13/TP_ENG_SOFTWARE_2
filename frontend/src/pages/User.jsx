8306918c79cc508dc321a8a467c480e3387d5ed1
FonsecaGabriela <gabrielaf2308@gmail.com>
2023-12-01 11:42:11 -0300

    Interface perfil do usuário

--- frontend/src/pages/User.jsx
+++ frontend/src/pages/User.jsx
@@ -1,29 +1,57 @@
 import React from 'react';
-import { Button, TextField, Typography, Container, CssBaseline, Paper } from '@mui/material';
+import { Typography, Paper } from '@mui/material';
 import { styled } from '@mui/system';
-
+import EmailIcon from '@mui/icons-material/Email';
+import PersonIcon from '@mui/icons-material/Person';
+import CakeIcon from '@mui/icons-material/Cake';
 
 const StyledPaper = styled(Paper)({
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  marginTop: '50%',
-  borderRadius: 20,
-  padding: '10%',
+    display: 'flex',
+    flexDirection: 'column',
+    margin: '7%',
+    marginLeft: '4%',
+    borderRadius: 20,
+    padding: '3%',
+    width: '100%',
+    height: '100%',
+    gap: 20
 });
 
+export const DataAlign = styled('div')({
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "left",
+    gap: 20
 
+});
 
 const User = () => {
     return (
-        <Container component="main" maxWidth="xs">
-        <CssBaseline />
         <StyledPaper elevation={3}>
-            <Typography component="h1" variant="h5">
-            User
-            </Typography>
+            <DataAlign>
+                <Typography component="h1" variant="h5">
+                    Meus Dados 
+                </Typography>
+            </DataAlign>
+            <DataAlign>
+                <PersonIcon/>
+                <Typography variant="body1">
+                    <strong>Nome:</strong> Gabriela Fonseca
+                </Typography>
+            </DataAlign>
+            <DataAlign>
+                <EmailIcon/>
+                <Typography variant="body1">
+                    <strong>Email:</strong> gabriela@gmail
+                </Typography>
+            </DataAlign>
+            <DataAlign>
+                <CakeIcon/>
+                <Typography variant="body1">
+                    <strong>Idade:</strong> 22
+                </Typography>
+            </DataAlign>
         </StyledPaper>
-        </Container>
     );
 };
 
