17f29af7ed95164653e6f31f996fdd5ba5986316
FonsecaGabriela <gabrielaf2308@gmail.com>
2023-12-01 10:50:45 -0300

    Interface deletar e adicionar conta e transação

---
+++ frontend/src/components/NewTransactionDialog.jsx
@@ -0,0 +1,85 @@
+import React, { useState } from 'react';
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button,
+  TextField,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+} from '@mui/material';
+
+const NewTransactionDialog = ({ open, onClose, onConfirm, nextTransactionId }) => {
+  const [newTransaction, setNewTransaction] = useState({
+    type: 'receita',
+    category: '',
+    account: '',
+    value: '',
+    date: '',
+  });
+
+  const handleFieldChange = (field, value) => {
+    setNewTransaction((prevTransaction) => ({ ...prevTransaction, [field]: value }));
+  };
+
+  const handleConfirm = () => {
+    // Adiciona o ID automático antes de confirmar
+    const transactionWithId = { ...newTransaction, id: nextTransactionId() };
+    onConfirm(transactionWithId);
+    onClose();
+  };
+
+  return (
+    <Dialog open={open} onClose={onClose}>
+      <DialogTitle>Nova Transação</DialogTitle>
+      <DialogContent>
+      <TextField
+          margin="normal"
+          fullWidth
+          label="Tipo"
+          value={newTransaction.type}
+          onChange={(e) => handleFieldChange('type', e.target.value)}
+        />
+        <TextField
+          margin="normal"
+          fullWidth
+          label="Categoria"
+          value={newTransaction.category}
+          onChange={(e) => handleFieldChange('category', e.target.value)}
+        />
+        <TextField
+          margin="normal"
+          fullWidth
+          label="Conta"
+          value={newTransaction.account}
+          onChange={(e) => handleFieldChange('account', e.target.value)}
+        />
+        <TextField
+          margin="normal"
+          fullWidth
+          label="Valor"
+          value={newTransaction.value}
+          onChange={(e) => handleFieldChange('value', e.target.value)}
+        />
+        <TextField
+          margin="normal"
+          fullWidth
+          type="date"
+          value={newTransaction.date}
+          onChange={(e) => handleFieldChange('date', e.target.value)}
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose}>Cancelar</Button>
+        <Button onClick={handleConfirm} color="primary">
+          Confirmar
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default NewTransactionDialog;
--- frontend/src/pages/Transactions.jsx
+++ frontend/src/pages/Transactions.jsx
@@ -1,141 +1,159 @@
 import React, { useState } from 'react';
 import {
-    Button,
-    Typography,
-    Container,
-    Paper,
-    Dialog,
-    DialogTitle,
-    DialogContent,
-    DialogActions,
+  Button,
+  Typography,
+  Paper,
+  Table,
+  TableBody,
+  TableRow,
+  TableCell,
+  TableHead,
+  TableContainer,
 } from '@mui/material';
 import { styled } from '@mui/system';
-import { DataGrid } from '@mui/x-data-grid';
+import DeleteConfirmationDialog from '../components/DeleteConfirmationDialog';
+import NewTransactionDialog from '../components/NewTransactionDialog';
 
 const StyledPaper = styled(Paper)({
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    margin: '8%',
-    marginLeft: '4%',
-    borderRadius: 20,
-    padding: '3%',
-    width: '100%',
-    height: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  margin: '7%',
+  marginLeft: '4%',
+  borderRadius: 20,
+  padding: '3%',
+  width: '100%',
+  height: '100%',
 });
 
 const transactionData = [
-    { id: 1, type: 'receita', category: 'lazer', account: '123456-7', value: 500.25, date: '2023-11-30' },
-    { id: 2, type: 'despesa', category: 'supermercado', account: '765432-1', value: 1200.75, date: '2023-11-29' },
-    { id: 3, type: 'receita', category: 'restaurante', account: '987654-3', value: 300.0, date: '2023-11-28' },
-    { id: 4, type: 'despesa', category: 'lazer', account: '987654-3', value: 300.0, date: '2023-11-27' },
+  { id: 1, type: 'receita', category: 'lazer', account: '123456-7', value: 500.25, date: '2023-11-30' },
+  { id: 2, type: 'despesa', category: 'supermercado', account: '765432-1', value: 1200.75, date: '2023-11-29' },
+  { id: 3, type: 'receita', category: 'restaurante', account: '987654-3', value: 300.0, date: '2023-11-28' },
+  { id: 4, type: 'despesa', category: 'lazer', account: '987654-3', value: 300.0, date: '2023-11-27' },
 ];
 
 const Transactions = () => {
-    const columns = [
-        { field: 'id', headerName: 'ID', width: 100 },
-        { field: 'type', headerName: 'Tipo', width: 120 },
-        { field: 'category', headerName: 'Categoria', width: 150 },
-        { field: 'account', headerName: 'Conta', width: 200 },
-        { field: 'value', headerName: 'Valor', width: 150 },
-        { field: 'date', headerName: 'Data', width: 150 },
-        {
-            field: 'actions',
-            headerName: 'Ações',
-            width: 150,
-            renderCell: (params) => (
-                <Button
+  const [deleteConfirmation, setDeleteConfirmation] = useState({
+    open: false,
+    transaction: null,
+  });
+
+  const [newTransactionDialog, setNewTransactionDialog] = useState({
+    open: false,
+  });
+
+  const nextTransactionId = () => transactionData.length + 1;
+
+  const handleOpenDeleteConfirmation = (transaction) => {
+    setDeleteConfirmation({
+      open: true,
+      transaction: transaction,
+    });
+  };
+
+  const handleCloseDeleteConfirmation = () => {
+    setDeleteConfirmation({
+      open: false,
+      transaction: null,
+    });
+  };
+
+  const handleConfirmDelete = () => {
+    // DELETAR TRANSAÇÃO
+    console.log(`Delete transaction with ID ${deleteConfirmation.transaction?.id}`);
+    handleCloseDeleteConfirmation();
+  };
+
+  const handleCancelDelete = () => {
+    handleCloseDeleteConfirmation();
+  };
+
+  const handleOpenNewTransactionDialog = () => {
+    setNewTransactionDialog({
+      open: true,
+    });
+  };
+
+  const handleCloseNewTransactionDialog = () => {
+    setNewTransactionDialog({
+      open: false,
+    });
+  };
+
+  const handleConfirmNewTransaction = (newTransaction) => {
+    // ADD NOVA TRANSAÇÃO NA LISTA
+    console.log('New transaction:', newTransaction);
+    handleCloseNewTransactionDialog();
+  };
+
+  return (
+    <StyledPaper elevation={3}>
+      <Typography component="h1" variant="h5">
+        Transações
+      </Typography>
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>ID</TableCell>
+              <TableCell>Tipo</TableCell>
+              <TableCell>Categoria</TableCell>
+              <TableCell>Conta</TableCell>
+              <TableCell>Valor</TableCell>
+              <TableCell>Data</TableCell>
+              <TableCell>Ações</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {transactionData.map((transaction) => (
+              <TableRow key={transaction.id}>
+                <TableCell style={{ width: '15%' }}>{transaction.id}</TableCell>
+                <TableCell style={{ width: '15%' }}>{transaction.type}</TableCell>
+                <TableCell style={{ width: '15%' }}>{transaction.category}</TableCell>
+                <TableCell style={{ width: '15%' }}>{transaction.account}</TableCell>
+                <TableCell style={{ width: '15%' }}>{transaction.value.toFixed(2)}</TableCell>
+                <TableCell style={{ width: '15%' }}>{transaction.date}</TableCell>
+                <TableCell style={{ width: '10%' }}>
+                  <Button
                     variant="contained"
                     color="error"
-                    onClick={() => handleOpenDeleteConfirmation(params.row.id)}
-                >
+                    onClick={() => handleOpenDeleteConfirmation(transaction)}
+                  >
                     Deletar
-                </Button>
-            ),
-        },
-    ];
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        <Button
+          variant="contained"
+          color="primary"
+          style={{
+            marginTop: '2%',
+          }}
+          onClick={handleOpenNewTransactionDialog}
+        >
+          Nova Transação
+        </Button>
+      </TableContainer>
 
-    const [deleteConfirmation, setDeleteConfirmation] = useState({
-        open: false,
-        transactionId: null,
-    });
+      <DeleteConfirmationDialog
+        open={deleteConfirmation.open}
+        onCancel={handleCancelDelete}
+        onConfirm={handleConfirmDelete}
+        itemText={`a transação ${deleteConfirmation.transaction?.id}`}
+      />
 
-    const handleOpenDeleteConfirmation = (transactionId) => {
-        setDeleteConfirmation({
-            open: true,
-            transactionId: transactionId,
-        });
-    };
-
-    const handleCloseDeleteConfirmation = () => {
-        setDeleteConfirmation({
-            open: false,
-            transactionId: null,
-        });
-    };
-
-    const handleConfirmDelete = () => {
-        // Implement logic to delete the transaction with the given ID
-        console.log(`Delete transaction with ID ${deleteConfirmation.transactionId}`);
-        handleCloseDeleteConfirmation();
-    };
-
-    const handleCancelDelete = () => {
-        handleCloseDeleteConfirmation();
-    };
-
-    const rows = transactionData.map((transaction) => ({
-        id: transaction.id,
-        type: transaction.type,
-        category: transaction.category,
-        account: transaction.account,
-        value: transaction.value.toFixed(2),
-        date: transaction.date,
-    }));
-
-    return (
-        <StyledPaper elevation={3}>
-            <Typography component="h1" variant="h5">
-                Transações
-            </Typography>
-
-            <div style={{ height: 400, width: '100%', margin: '2%' }}>
-                <DataGrid
-                    rows={rows}
-                    columns={columns}
-                    pageSize={5}
-                    padding="center"
-                />
-            </div>
-
-            <Button
-                variant="contained"
-                color="primary"
-                style={{
-                    marginTop: '2%',
-                }}
-            >
-                Nova Transação
-            </Button>
-
-            <Dialog open={deleteConfirmation.open} onClose={handleCloseDeleteConfirmation}>
-                <DialogTitle>Confirmar Exclusão</DialogTitle>
-                <DialogContent>
-                    <Typography>
-                        Deseja deletar esta transação?
-                    </Typography>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleCancelDelete} color="primary">
-                        Cancelar
-                    </Button>
-                    <Button onClick={handleConfirmDelete} variant="contained" color="error">
-                        Deletar
-                    </Button>
-                </DialogActions>
-            </Dialog>
-        </StyledPaper>
-    );
+      <NewTransactionDialog
+        open={newTransactionDialog.open}
+        onClose={handleCloseNewTransactionDialog}
+        onConfirm={handleConfirmNewTransaction}
+        nextTransactionId={nextTransactionId}
+      />
+    </StyledPaper>
+  );
 };
 
 export default Transactions;
