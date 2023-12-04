085f1efef05be9aa07949afdea7f2ed2204310d1
Gabriel Valadão Meira <gabriel.valadao@sedecam>
2023-12-03 22:01:35 -0300

    testes integração account

--- backend/tests/integration/AccountServiceIntegrationTeste.test.js
+++ backend/tests/integration/AccountServiceIntegrationTeste.test.js
@@ -7,6 +7,7 @@ const Transaction = require('../../database/Models/Transaction');
 const superTest = require('supertest');
 const app = require('../../app');
 const request = superTest.agent(app);
+let id=0;
 
 describe('testeIntegracaoAccount', () => {
     test('testLogins', async () => {
@@ -14,8 +15,8 @@ describe('testeIntegracaoAccount', () => {
         const response = await request
             .post('/api/usuarios/login')
             .send({
-                email: "gabriel@gmail",
-                password: "gabriel123",
+                email: "admin@gmail.com",
+                password: "12345678",
 
             });
             expect(response.statusCode).toBe(204);
@@ -30,9 +31,57 @@ describe('testeIntegracaoAccount', () => {
                 agency: "inter",
                 balance: 1000,
             });
+            id=response.body.id;
 
         //console.log(response);
         expect(response.statusCode).toBe(201);
+    });
+
+    test('listAccounts', async () => {
+        const response = await request
+            .get('/api/contas/listarContas');
+
+        //console.log(response);
+        expect(response.statusCode).toBe(200);
+    });
+
+    test('listAccountById1', async () => {
+        const response = await request
+            .get(`/api/contas/conta/${id}`);
+
+        //console.log(response);
+        expect(response.statusCode).toBe(200);
+    });
+
+
+    test('listAccountByUserId', async () => {
+        const response = await request
+            .get(`/api/contas/conta/usuario/:id`);
+
+        //console.log(response);
+        expect(response.statusCode).toBe(200);
+    });
+
+    test('updateAccountById', async () => {
+        const updatedAccount = {
+            agency: "inter",
+            balance: 1000,
+        };
+    
+        const response = await request
+            .put(`/api/contas/conta/${id}`)
+            .send(updatedAccount);
+
+        //console.log(response);
+        expect(response.statusCode).toBe(200);
+    });
+
+    test('deleteAccount', async () => {
+        const response = await request
+            .delete(`/api/contas/conta/${id}`);
+
+        //console.log(response);
+        expect(response.statusCode).toBe(204);
     });
 
     
